920f018a07fc3097e6dc7338ae483d434773c7e5
Alex Kumok <lkrfan@gmail.com>
2020-10-07 20:15:16 -0500

    End of Section 7

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,26 +1,15 @@
-import React, { SyntheticEvent } from 'react'
+import { observer } from 'mobx-react-lite';
+import React, { useContext } from 'react'
 import { Button, Item, Label, Segment } from 'semantic-ui-react';
-import { IActivity } from '../../../app/models/activity';
+import ActivityStore from '../../../app/stores/activityStore';
 
-interface IProps {
-    activities: IActivity[];
-    selectActivity: (id: string) => void;
-    deleteActivity: (event: SyntheticEvent<HTMLButtonElement>,id: string) => void;
-    submitting: boolean;
-    target: string;
-}
-
-export const ActivityList: React.FC<IProps> = ({
-    activities, 
-    selectActivity, 
-    deleteActivity, 
-    submitting,
-    target
-    }) => {
+const ActivityList: React.FC = () => {
+    const activityStore = useContext(ActivityStore)
+    const {activitiesByDate, selectActivity, deleteActivity, submitting, target} = activityStore;
     return (
         <Segment clearing>
             <Item.Group divided>
-                {activities.map(activity => (
+                {activitiesByDate.map(activity => (
                     <Item key={activity.id}>    
                         <Item.Content>
                             <Item.Header as='a'>{activity.title}</Item.Header>
@@ -31,6 +20,7 @@ export const ActivityList: React.FC<IProps> = ({
                             </Item.Description>
                             <Item.Extra>
                                 <Button 
+                                    // onClick={() => console.log(activity.id)}
                                     onClick={() => selectActivity(activity.id)} 
                                     floated='right' 
                                     content='View' 
@@ -54,3 +44,5 @@ export const ActivityList: React.FC<IProps> = ({
 
     )
 }
+
+export default observer(ActivityList);
